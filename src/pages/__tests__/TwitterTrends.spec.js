57e1ba2ebfad794ed8c146fc69f3cf1710474487
Ocki Bagus Pratama <ocki.bagus.p@gmail.com>
2022-10-14 19:59:00 +0700

    [14/10/22] textarea `hasil` for array for trends: unchecked

--- src/pages/__tests__/TwitterTrends.spec.js
+++ src/pages/__tests__/TwitterTrends.spec.js
@@ -246,70 +246,78 @@ describe('Twitter Trends API', async() => {
     await btnCheckBoxAll.trigger('click')
     assert.equal(btnCheckBoxAll.text(), 'tidak diaktifkan')
     assert.equal(hasil.element.value, 'Tags: #Test1, #Test2, #Test3, #Test4, #Test5, Test 6, Test 7, Test 8, Test 9, Test 10, Test 11, Test 12, Test 13, Test 14, Test 15')
+    assert.equal(btnTweet.text(), 'Tweet is: + 150')
   })
 
-  // it('textarea `hasil` untuk array untuk trends: tidak dicentang', async() => {
-  //   await copydanpaste.trigger('change')
-        
-  //   assert.equal(hasil.element.value, 'Tags: #TimnasIndonesia, Test 1, #Test2, Test 3')
-  //   assert.equal(btnTweet.text(), 'Tweet is: + 234')
+  it('textarea `hasil` untuk array untuk trends: tidak dicentang', async() => {
+    // test cases
+    const testCases = [
+      {
+        name: '#Test1',
+        index: 0,
+        listBool: [false, true, true, true],
+        hasil: 'Tags: #Test2, #Test3, #Test4, #Test5, Test 6, Test 7, Test 8, Test 9, Test 10, Test 11, Test 12, Test 13, Test 14, Test 15',
+        tweetIs: 'Tweet is: + 158',
+        // `semua kotak centang` diaktifkan
+        allCheckboxesEnabled: 'diaktifkan: 14'
+      },
+      {
+        name: '#Test2',
+        index: 1,
+        listBool: [false, false, true, true],
+        hasil: 'Tags: #Test3, #Test4, #Test5, Test 6, Test 7, Test 8, Test 9, Test 10, Test 11, Test 12, Test 13, Test 14, Test 15',
+        tweetIs: 'Tweet is: + 166',
+        allCheckboxesEnabled: 'diaktifkan: 13'
+      },
+      {
+        name: '#Test3',
+        index: 2,
+        listBool: [false, false, false, true],
+        hasil: 'Tags: #Test4, #Test5, Test 6, Test 7, Test 8, Test 9, Test 10, Test 11, Test 12, Test 13, Test 14, Test 15',
+        tweetIs: 'Tweet is: + 174',
+        allCheckboxesEnabled: 'diaktifkan: 12'
+      },
+      {
+        name: '#Test4',
+        index: 3,
+        listBool: [false, false, false, false],
+        hasil: 'Tags: #Test5, Test 6, Test 7, Test 8, Test 9, Test 10, Test 11, Test 12, Test 13, Test 14, Test 15',
+        tweetIs: 'Tweet is: + 182',
+        allCheckboxesEnabled: 'diaktifkan: 11'
+      }
+    ]
 
-  //   // test cases
-  //   const testCases = [
-  //     {
-  //       name: '#TimnasIndonesia',
-  //       index: 0,
-  //       listBool: [false, true, true, true],
-  //       hasil: 'Tags: Test 1, #Test2, Test 3',
-  //       tweetIs: 'Tweet is: + 252',
-  //       // `semua kotak centang` diaktifkan
-  //       allCheckboxesEnabled: 'diaktifkan: 3'
-  //     },
-  //     {
-  //       name: 'Test 1',
-  //       index: 1,
-  //       listBool: [false, false, true, true],
-  //       hasil: 'Tags: #Test2, Test 3',
-  //       tweetIs: 'Tweet is: + 260',
-  //       allCheckboxesEnabled: 'diaktifkan: 2'
-  //     },
-  //     {
-  //       name: '#Test2',
-  //       index: 2,
-  //       listBool: [false, false, false, true],
-  //       hasil: 'Tags: Test 3',
-  //       tweetIs: 'Tweet is: + 268',
-  //       allCheckboxesEnabled: 'diaktifkan: 1'
-  //     },
-  //     {
-  //       name: 'Test 3',
-  //       index: 3,
-  //       listBool: [false, false, false, false],
-  //       hasil: 'Tidak ada hasil',
-  //       tweetIs: 'Tweet is: + 280',
-  //       allCheckboxesEnabled: 'diaktifkan: 0'
-  //     }
-  //   ]
-
-  //   for (let test of testCases) {
-  //     console.debug('unchecked ke-', test.name)
-  //     await checkboxTrends.at(test.index).setValue(false)
+    for (let test of testCases) {
+      console.debug('unchecked ke-', test.name)
+      await checkboxTrends.at(test.index).setValue(false)
       
-  //     for (let i = 0; i < test.listBool.length; i++) {
-  //       if (test.listBool[i]) {
-  //         expect(arrayTrends.at(i).classes()).toContain('completed')
-  //       } else {
-  //         // same: assert.deepEqual(arrayTrends.at(...).classes(), [])
-  //         expect(arrayTrends.at(i).classes()).to.deep.equal([])
-  //       }
-  //     }
+      for (let i = 0; i < test.listBool.length; i++) {
+        if (test.listBool[i]) {
+          expect(arrayTrends.at(i).classes()).toContain('completed')
+        } else {
+          // same: assert.deepEqual(arrayTrends.at(...).classes(), [])
+          expect(arrayTrends.at(i).classes()).to.deep.equal([])
+        }
+      }
 
-  //     assert.equal(hasil.element.value, test.hasil)
-  //     assert.equal(btnTweet.text(), test.tweetIs)
-  //     // `semua kotak centang` diaktifkan
-  //     assert.equal(allCheckboxesEnabled.text(), test.allCheckboxesEnabled)
-  //   }
-  // })
+      assert.equal(hasil.element.value, test.hasil)
+      assert.equal(btnTweet.text(), test.tweetIs)
+      // `semua kotak centang` diaktifkan
+      assert.equal(allCheckboxesEnabled.text(), test.allCheckboxesEnabled)
+    }
+
+    console.debug('unchecked ke-5 sd. ke-15')
+    for (let i = 4; i < 15; i++) {
+      await checkboxTrends.at(i).setValue(false)
+      // same: assert.deepEqual(arrayTrends.at(...).classes(), [])
+      expect(arrayTrends.at(i).classes()).to.deep.equal([])
+    }
+
+    assert.equal(hasil.element.value, 'Tidak ada hasil')
+    assert.equal(btnTweet.text(), 'Tweet is: + 280')
+    // `semua kotak centang` diaktifkan
+    assert.equal(allCheckboxesEnabled.text(), 'diaktifkan: 0')
+  })
 
   // it('textarea `hasil` untuk array untuk trends: dicentang', async() => {    
   //   console.debug('-----')
