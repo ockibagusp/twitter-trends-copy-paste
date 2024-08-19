2c353f50c3aba90ff49e7fdd258a73e89e7d8a2d
Ocki Bagus Pratama <ocki.bagus.p@gmail.com>
2024-08-19 14:16:19 +0700

    fix library ts

---
+++ src/library/__tests__/library.test.ts
@@ -0,0 +1,211 @@
+import { describe, test, expect, vi, afterEach, beforeEach, type MockedClass } from 'vitest'
+import { RegExpYouTube } from '../library'
+
+vi.mock('axios')
+
+const YOUTUBE_SUCCESS01 = 'https://www.youtube.com/watch?v=success-01'
+const YOUTUBE_SUCCESS02 = 'https://www.youtube.com/shorts/success-02'
+const YOUTUBE_FAILURE = 'https://www.youtube.com/watch?v=failure-01'
+// vi.mock('../library', async (importOriginal) => {
+//   // const actual = await importOriginal()
+//   const actual = vi.requireActual('../library')
+//
+//   return {
+//     ...actual,
+//     RegExpYouTube: vi.fn().mockReturnValue({
+//       constructor: vi.fn(),
+//       setNewTweet: vi.fn(),
+//       // link: Expected 0 arguments, but got 1'
+//       getNewTweet: vi.fn((link) => {
+//         if (link == 1) return YOUTUBE_SUCCESS01
+//         else if (link == 2) return YOUTUBE_SUCCESS02
+//         else if (link == 3) return YOUTUBE_FAILURE
+//         else return 'mock'
+//       })
+//     })
+//   }
+// })
+vi.mock('../library')
+
+window.alert = vi.fn()
+
+describe('library mount component', async () => {
+  let regExpYouTube: RegExpYouTube
+
+  beforeEach(() => {
+    regExpYouTube = new RegExpYouTube(YOUTUBE_SUCCESS01)
+  })
+
+  afterEach(() => {
+    // regExpYouTube = null
+    vi.resetAllMocks()
+    vi.clearAllMocks()
+  })
+
+  test('defines constructor()', () => {
+    const regExpYouTube = new RegExpYouTube()
+    expect(typeof regExpYouTube.constructor).toBe('function')
+  })
+
+  // https://soorria.com/snippets/mocking-classes-vitest
+  test('should calls setNewTweet(...) and constructor(?:...)', async () => {
+    const regExpCustom1 = new RegExpYouTube(YOUTUBE_SUCCESS01)
+    let getNewTweetMock = vi.mocked(regExpCustom1.getNewTweet).mockReturnValue(YOUTUBE_SUCCESS01)
+    let getNewTweet = getNewTweetMock()
+    expect(getNewTweetMock).toBeCalledTimes(1)
+    expect(getNewTweet).toEqual(YOUTUBE_SUCCESS01)
+
+    const regExpCustom2 = new RegExpYouTube()
+    regExpCustom2.setNewTweet(YOUTUBE_SUCCESS01)
+    getNewTweetMock = vi.mocked(regExpCustom2.getNewTweet).mockReturnValue(YOUTUBE_SUCCESS01)
+    getNewTweet = getNewTweetMock()
+    expect(getNewTweetMock).toBeCalledTimes(2)
+    expect(getNewTweet).toEqual(YOUTUBE_SUCCESS01)
+
+    const regExpCustom3 = new RegExpYouTube(YOUTUBE_SUCCESS02)
+    getNewTweetMock = vi.mocked(regExpCustom3.getNewTweet).mockReturnValue(YOUTUBE_SUCCESS02)
+    getNewTweet = getNewTweetMock()
+    expect(getNewTweetMock).toBeCalledTimes(3)
+    expect(getNewTweet).toEqual(YOUTUBE_SUCCESS02)
+
+    const regExpCustom4 = new RegExpYouTube()
+    regExpCustom4.setNewTweet(YOUTUBE_SUCCESS02)
+    getNewTweetMock = vi.mocked(regExpCustom4.getNewTweet).mockReturnValue(YOUTUBE_SUCCESS02)
+    getNewTweet = getNewTweetMock()
+    expect(getNewTweetMock).toBeCalledTimes(4)
+    expect(getNewTweet).toEqual(YOUTUBE_SUCCESS02)
+  })
+
+  // Mock vs Spy in Testing with Jest: Which is Better?: https://www.youtube.com/watch?v=9N8D7U9Am8o
+  test('should return Youtube watching: success', async () => {
+    // getYoutubeInVideoTitle()
+    const titleMock = 'Test One - Author One Two Three'
+    const isYoutubeOnTitleMock = vi
+      .mocked(regExpYouTube.getYoutubeInVideoTitle)
+      .mockResolvedValue(titleMock)
+    const isYoutubeOnTitle = await isYoutubeOnTitleMock()
+    expect(isYoutubeOnTitleMock).toBeCalledTimes(1)
+    expect(isYoutubeOnTitle).toEqual(titleMock)
+  })
+
+  test('should return Youtube watching: failure', async () => {
+    /**
+     * getYoutubeInVideoTitle()
+     * */
+    // try: AxiosResponse res.status
+    let errorMock = 'YouTube not found'
+    let isYoutubeOnTitleMock = vi
+      .mocked(regExpYouTube.getYoutubeInVideoTitle)
+      .mockResolvedValue(errorMock)
+    let isYoutubeOnTitle = await isYoutubeOnTitleMock()
+    expect(isYoutubeOnTitleMock).toBeCalledTimes(1)
+    expect(isYoutubeOnTitle).toEqual(errorMock)
+
+    /**
+     * objYoutubeInVideoTitle(..., res.data)
+     */
+    // try: title instanceof Error
+    errorMock = 'objYoutubeInVideoTitle: YouTube title not found'
+    isYoutubeOnTitleMock = vi
+      .mocked(regExpYouTube.getYoutubeInVideoTitle)
+      .mockResolvedValue(errorMock)
+    isYoutubeOnTitle = await isYoutubeOnTitleMock()
+    expect(isYoutubeOnTitleMock).toBeCalledTimes(2)
+    expect(isYoutubeOnTitle).toEqual(errorMock)
+
+    // catch
+    expect.assertions(4)
+    try {
+      await isYoutubeOnTitleMock()
+    } catch (error) {
+      expect(error).toMatch('error')
+    }
+  })
+
+  test.skip('should throw catch on Youtube watching: failure', async () => {
+    // catch
+    const errorMock = 'objYoutubeInVideoTitle: YouTube title not found'
+    const isYoutubeOnTitleMock = vi
+      .mocked(regExpYouTube.getYoutubeInVideoTitle)
+      .mockResolvedValue(errorMock)
+
+    expect.assertions(1)
+    try {
+      await isYoutubeOnTitleMock()
+    } catch (error) {
+      expect(error).toMatch('error')
+    }
+  })
+
+  test('should return text in full video url on function getYoutubeInFullVideoUrl(): success', () => {
+    const successMock = 'https://www.youtube.com/watch?v=success'
+    const getYoutubeInFullVideoUrlMock = vi
+      .mocked(regExpYouTube.getYoutubeInFullVideoUrl)
+      .mockReturnValue(successMock)
+    const getYoutubeInFullVideoUrl = getYoutubeInFullVideoUrlMock()
+    expect(getYoutubeInFullVideoUrl).toBe(successMock)
+  })
+
+  test('should return text in full video url on function getYoutubeInFullVideoUrl(): failure', async () => {
+    const errorMock = 'not found this this.isYoutube()'
+    const getYoutubeInFullVideoUrlMock = vi
+      .mocked(regExpYouTube.getYoutubeInFullVideoUrl)
+      .mockRejectedValue(new Error(errorMock))
+    const getYoutubeInFullVideoUrl = getYoutubeInFullVideoUrlMock()
+    expect(getYoutubeInFullVideoUrl).rejects.toThrow(errorMock)
+  })
+
+  test('should return text in video url on function getYoutubeInVideoUrl(): success', () => {
+    const successMock = 'youtube.com/00-success'
+    const getYoutubeInVideoUrlMock = vi
+      .mocked(regExpYouTube.getYoutubeInVideoUrl)
+      .mockReturnValue(successMock)
+    const getYoutubeInVideoUrl = getYoutubeInVideoUrlMock()
+    expect(getYoutubeInVideoUrl).toBe(successMock)
+  })
+
+  test('should return text in video url on function getYoutubeInVideoUrl(): failure', async () => {
+    const errorMock = 'not found this this.isYoutube()'
+    const getYoutubeInVideoUrlMock = vi
+      .mocked(regExpYouTube.getYoutubeInVideoUrl)
+      .mockRejectedValue(new Error(errorMock))
+    const getYoutubeInVideoUrl = getYoutubeInVideoUrlMock()
+    expect(getYoutubeInVideoUrl).rejects.toThrow(errorMock)
+  })
+
+  test('should return true in Youtube video on function isYoutube(): success', async () => {
+    const isYoutubeMock = vi.mocked(regExpYouTube.isYoutube).mockResolvedValue(true)
+    const isYoutube = await isYoutubeMock()
+    expect(isYoutubeMock).toHaveBeenCalledTimes(1)
+    expect(isYoutube).toBe(true)
+    isYoutubeMock.mockRestore()
+  })
+
+  test('should return false in Youtube video on function isYoutube(): failure', async () => {
+    const isYoutubeMock = vi.mocked(regExpYouTube.isYoutube).mockResolvedValue(false)
+    const isYoutube = await isYoutubeMock()
+    expect(isYoutubeMock).toHaveBeenCalledTimes(1)
+    expect(isYoutube).toBe(false)
+    isYoutubeMock.mockRestore()
+  })
+
+  test.skip('should return { full, watch, video } on function objYoutubeInVideoUrl(...): success', async () => {
+    // const objYoutubeInVideoUrlMock = vi.mocked(regExpYouTube.??)
+    // regExpYouTubeMock.mockRejectedValue({
+    //   full: 'https://www.youtube.com/shorts/00-success',
+    //   watch: '/shorts/',
+    //   video: '00-success'
+    // })
+    // const message = 'regExpAndExec: not found'
+    // mockAxios.get.mockResolvedValue({ data: 'test' })
+    // expect(mockAxios.get).toHaveBeenCalledWith(1)
+    // // expect(regExpCustom.isYoutubeInVideoUrl()).toBeTruthy()
+    // // expect(regExpCustom.getYoutubeInFullVideoUrl()).toEqual(YOUTUBE_SUCCESS01)
+    // console.log(regExpYouTube);
+    // //expect(regExpYouTube.getYoutubeInVideoUrl()).toEqual('youtu.be/peUj47yc1xo')
+    // const titleOnYouTube = await regExpYouTube.getYoutubeInVideoTitle()
+    // expect(titleOnYouTube).toEqual('DOSA - Ustadz Dr. Firanda Andirja, MA')
+    // const mockResponse = { data: {}, status: 400 }
+    // axios.get.mockResolvedValueOnce(mockResponse)
+  })
+})
--- src/library/library.ts
+++ src/library/library.ts
@@ -1,9 +1,10 @@
 import { YOUTUBE, REGEXYOUTUBEVIDEO, REGEXYOUTUBEVIDEOTITLE } from '@/utils/utils'
 
 import axios from 'axios'
-const REDIRCETURL = 'http://localhost:3000/youtube'
 
-export class RegExpCustom {
+const REDIRCETURL = '/youtube'
+
+export class RegExpYouTube {
   newTweet!: string
   private youtubeUrls!: {
     full: string
@@ -25,29 +26,23 @@ export class RegExpCustom {
   }
 
   public getYoutubeInFullVideoUrl() {
-    if (this.youtubeUrls == undefined) throw new Error('not found this this.isYoutubeVideo()')
+    if (this.youtubeUrls == undefined) throw new Error('not found this this.isYoutube()')
     return this.youtubeUrls.full
   }
 
   public getYoutubeInVideoUrl() {
-    // if (
-    //   this.youtubeUrls.full == undefined &&
-    //   this.youtubeUrls.watch == undefined &&
-    //   this.youtubeUrls.video == undefined
-    // )
-    //   throw new Error('not found this this.isYoutubeVideo()')
+    if (
+      this.youtubeUrls.full == undefined &&
+      this.youtubeUrls.watch == undefined &&
+      this.youtubeUrls.video == undefined
+    )
+      throw new Error('not found this this.isYoutube()')
     return YOUTUBE + '/' + this.youtubeUrls.video
   }
 
-  public getYoutubeInVidoeTitle() {
-    if (this.youtubeInVideoTitle == undefined)
-      throw new Error('not found this this.isYoutubeVideo()')
-    return this.youtubeInVideoTitle
-  }
-
-  public async isYoutubeInVideoUrl(): Promise<boolean> {
-    const regex = this.objYoutubeInVideoUrl(REGEXYOUTUBEVIDEO, this.newTweet)
-    if (regex == undefined) return false
+  public async isYoutube(): Promise<boolean> {
+    const regex = this.objYoutubeInVideoUrl(REGEXYOUTUBEVIDEO)
+    if (regex instanceof Error) return false
 
     this.youtubeUrls = {
       full: regex.full,
@@ -58,64 +53,79 @@ export class RegExpCustom {
     return true
   }
 
-  public async getYoutubeInVideoTitle(): Promise<string | null> {
-    const youtubeWatch: string = REDIRCETURL + this.youtubeUrls.watch + this.youtubeUrls.video
-    const res = await axios.get(youtubeWatch)
-
-    if (res.status == 404 || res.data.search(`This video isn't available anymore`) !== -1) {
-      console.debug('getYoutubeVideo: error not found')
-      return null
-    }
-
-    const title = this.objYoutubeInVideoTitle(REGEXYOUTUBEVIDEOTITLE, res.data)
-    if (title == null) {
-      console.debug('getYoutubeVideo: error not found')
-      return null
-    }
-    return title
+  public async getYoutubeInVideoTitle(): Promise<string | Error> {
+    if (this.youtubeInVideoTitle != undefined) return this.youtubeInVideoTitle
+    return this.getYoutubeWatch()
   }
 
-  private objYoutubeInVideoUrl(
-    regexExp: RegExp,
-    regexExec: string
-  ): {
-    full: string
-    watch: string
-    video: string
-  } | null {
+  private async getYoutubeWatch(): Promise<string | Error> {
     try {
-      const regExpExecArray = this.regExpAndExec(regexExp, regexExec)
+      const youtubeWatch: string = REDIRCETURL + this.youtubeUrls.watch + this.youtubeUrls.video
+      const res = await axios.get(youtubeWatch)
+      if (res.status == 404 || res.data.search(`This video isn't available anymore`) !== -1) {
+        throw new Error('YouTube not found')
+      }
 
-      const full = regExpExecArray?.at(0)
-      const watch = regExpExecArray?.at(1)
-      const video = regExpExecArray?.at(2)
+      const title = this.objYoutubeInVideoTitle(REGEXYOUTUBEVIDEOTITLE, res.data)
+      if (title instanceof Error) throw title
+      this.youtubeInVideoTitle = title
+      return title
+    } catch (error: any) {
+      console.error('An error occurred:', error)
+      throw error
+    }
+  }
 
-      if (full == undefined || watch == undefined || video == undefined) return null
+  private objYoutubeInVideoUrl(regexExp: RegExp):
+    | {
+        full: string
+        watch: string
+        video: string
+      }
+    | Error {
+    try {
+      const regExpExecArray = this.regExpAndExec(regexExp, this.newTweet)
+      if (regExpExecArray instanceof Error) throw regExpExecArray
+
+      const full = regExpExecArray.at(0)
+      const watch = regExpExecArray.at(1)
+      const video = regExpExecArray.at(2)
+
+      if (full == undefined || watch == undefined || video == undefined)
+        throw new Error('objYoutubeInVideoUrl: YouTube not found')
       return {
         full: full,
         watch: watch,
         video: video
       }
     } catch (error: any) {
-      return error
+      console.error('An error occurred:', error)
+      throw error
     }
   }
 
-  private objYoutubeInVideoTitle(regexExp: RegExp, regexExec: string): string | null {
-    const regExpExecArray = this.regExpAndExec(regexExp, regexExec)
+  private objYoutubeInVideoTitle(regexExp: RegExp, regexExec: string): string | Error {
+    try {
+      const regExpExecArray = this.regExpAndExec(regexExp, regexExec)
+      if (regExpExecArray instanceof Error) throw regExpExecArray
 
-    const full = regExpExecArray?.at(0)
-    const parent = regExpExecArray?.at(1)
+      const full = regExpExecArray.at(0)
+      const parent = regExpExecArray.at(1)
 
-    if (full == undefined || parent == undefined) return null
-    return parent
+      if (full == undefined || parent == undefined)
+        throw new Error('objYoutubeInVideoTitle: YouTube title not found')
+      return parent
+    } catch (error: any) {
+      console.error('An error occurred:', error)
+      throw error
+    }
   }
 
-  private regExpAndExec(regexExp: RegExp, regexExec: string): RegExpExecArray | null {
+  private regExpAndExec(regexExp: RegExp, regexExec: string): RegExpExecArray | Error {
     const regex = RegExp(regexExp, 'gm')
     const regExpExecArray = regex.exec(regexExec)
 
-    if (regExpExecArray == null) return null
+    if (regExpExecArray == null) throw new Error('regExpAndExec: not found')
     return regExpExecArray
   }
 }
